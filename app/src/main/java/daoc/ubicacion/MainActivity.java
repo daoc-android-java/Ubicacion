25f54fd4775f1da3885148445ffd0cc8d19dd366
Diego Ordóñez <diego.ordonez@gmail.com>
2020-07-13 19:13:16 -0500

    Update

--- app/src/main/java/daoc/ubicacion/MainActivity.java
+++ app/src/main/java/daoc/ubicacion/MainActivity.java
@@ -1,8 +1,10 @@
 package daoc.ubicacion;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -13,28 +15,31 @@ import android.webkit.WebViewClient;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
+
 // http://developer.android.com/training/location/index.html
 public class MainActivity extends Activity {
 
 	private LocationManager locationManager;
 	private TextView ubicacion;
 	private WebView web;
-	
+
 	// Este ejemplo usa WebView. Se puede hacer mucho mas con la API de mapas:
 	// https://developers.google.com/maps/
-	@SuppressLint("SetJavaScriptEnabled") @Override
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		ubicacion = (TextView) findViewById(R.id.ubicacion);
-			
+
 		web = (WebView) findViewById(R.id.web);
 		web.setWebViewClient(new WebViewClient());
 		web.getSettings().setJavaScriptEnabled(true);
-		web.loadUrl("http://www.android.com");		
+		web.loadUrl("https://www.android.com");
 	}
-	
+
 	//onClick definido para el boton en el archivo de layout
 	public void iniciaUbicar(View v) {
 		//En el emulador puede simular la ubicacion asi:
@@ -42,15 +47,15 @@ public class MainActivity extends Activity {
 		//geo fix <longitud> <latitud>
 		//Ej Quito: geo fix -0.18 -78.5
 		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		
-		if(locationManager == null) {
+
+		if (locationManager == null) {
 			Toast.makeText(this, "No hay GPS en el dispositivo", Toast.LENGTH_SHORT).show();
-		} else if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+		} else if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10, 10, locationListener);
 			Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 			muestraUbicacion(location);
 		} else {
-			Toast.makeText(this, "El GPS no est� habilitado", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "El GPS no está habilitado", Toast.LENGTH_SHORT).show();
 		}		
 	}
 	
@@ -72,8 +77,8 @@ public class MainActivity extends Activity {
 			Toast.makeText(this, "Ubicacion obtenida", Toast.LENGTH_SHORT).show();
 			ubicacion.setText("Long: " + location.getLongitude() + " Lat: " + location.getLatitude());
 			String url = "https://www.google.com/maps/@" + 
-				location.getLongitude() + "," + 
-				location.getLatitude() + ",10z"; // z es el nivel de zoom
+				location.getLatitude() + "," +
+				location.getLongitude() + ",15z"; // z es el nivel de zoom
 			web.loadUrl(url);
 		} else {
 			Toast.makeText(this, "El GPS aun no obtiene la ubicacion", Toast.LENGTH_SHORT).show();
